44b0d8b41aa5ee652479712e45cd38dfa22b8915
nobumichi asai <nobumichiasai@gmail.com>
2025-05-08 20:50:00 +0900

    パス変更

--- _generate_movie_with_contrail_detection.py
+++ _generate_movie_with_contrail_detection.py
@@ -2,6 +2,6 @@ from config import *
 from _movie_generator import generate_movie    
 
 file_name = 'timelasp_movie_suma_contrai_detection'
-input_dir = os.path.join(IMAGE_ANALYSIS_DIR, 'output_img_path')
+input_dir = os.path.join(IMAGE_ANALYSIS_DIR, 'suma/output_image')
 output_dir = MOVIE_DIR
 generate_movie(input_dir, output_dir, file_name, time_stamp=True)
